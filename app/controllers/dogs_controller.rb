28171c29f7821f8f07098026244d68c06617303d
milanla <milan.lanlada@gmail.com>
2018-10-18 16:23:12 -0400

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,13 @@
 class DogsController < ApplicationController
+  def index
+    if params[:query]
+      @dogs = Dog.all.sort_by { |dog| dog.employees.count }.reverse
+    else
+      @dogs = Dog.all
+    end
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :title, uniqueness: true
+  validates :alias, uniqueness: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
